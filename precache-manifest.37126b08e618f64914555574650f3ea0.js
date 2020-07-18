0b6ac096a9809342d33efd40642162743bde0d02
Linawaty <linawatylim@gmail.com>
2020-07-18 12:40:49 +0800

    Updates

---
+++ precache-manifest.37126b08e618f64914555574650f3ea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b574cade9888279c2ea84b845f10ee13",
+    "url": "/taco-react/index.html"
+  },
+  {
+    "revision": "843113e69b6ba58be462",
+    "url": "/taco-react/static/css/main.ef50d4ab.chunk.css"
+  },
+  {
+    "revision": "39fc99692bab2b88abae",
+    "url": "/taco-react/static/js/2.01605af9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taco-react/static/js/2.01605af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843113e69b6ba58be462",
+    "url": "/taco-react/static/js/main.7d532014.chunk.js"
+  },
+  {
+    "revision": "9d26d31f3dfd164a05a0",
+    "url": "/taco-react/static/js/runtime-main.3320e33d.js"
+  }
+]);
