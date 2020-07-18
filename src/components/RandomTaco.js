6f0187dda36f3c584800bb7300dc11726d192f94
Linawaty <linawatylim@gmail.com>
2020-07-18 11:17:24 +0800

    Create first component

---
+++ src/components/RandomTaco.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class RandomTaco extends Component {
+    render() {
+        return (
+            <div>
+                Random Tacos here
+            </div>
+        )
+    }
+}
+
+export default RandomTaco
