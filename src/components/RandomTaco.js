a15639eb902fbdc932781578ee4bffc36bb77ea4
Linawaty <linawatylim@gmail.com>
2020-07-18 12:03:55 +0800

    Connected to API

--- src/components/RandomTaco.js
+++ src/components/RandomTaco.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
 
 export class RandomTaco extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            taco: null,
+        };
+    }
+    componentDidMount() {
+        const apiUrl = 'http://taco-randomizer.herokuapp.com/random/';
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Taco:', data);
+                this.setState({ taco: data });
+            });
+    }
     render() {
-        return (
-            <div>
-                Random Tacos here
-            </div>
-        )
+        if (this.state.taco === null) {
+            return (
+                <div>Finding...</div>
+            )
+        } else {
+            return (
+                <div>
+                    <div>Seasoning: {this.state.taco.seasoning.name}</div>
+                    <div>Base Layer: {this.state.taco.base_layer.name}</div>
+                    <div>Mixin: {this.state.taco.mixin.name}</div>
+                    <div>Shell: {this.state.taco.shell.name}</div>
+                    <div>Condiment: {this.state.taco.condiment.name}</div>
+                </div>
+            )
+        }
+
     }
 }
 
